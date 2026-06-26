66dec5dc1bbbfa4e37e82debdba1553647d880eb
Ashish <ashishkaushik0904@gmail.com>
2026-06-26 22:29:22 +0530

    Fix remaining Vercel build issues

--- frontend/src/landing_page/products/Hero.js
+++ frontend/src/landing_page/products/Hero.js
@@ -1,15 +1,32 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Hero() {
-    return ( 
-        <div className='container'>
-            <div className='row text-center mt-5 mb-2'>
-                <h1>Technology</h1>
-                <h5 className='text-muted mt-2'>Sleek, modern and intutive platform</h5> <br/> <br/>
-                <p>Check out our <a href='#' style={{textDecoration:"none"}}>Investment offerings  <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a></p>
-            </div>
-        </div>
-     );
+  return (
+    <div className="container">
+      <div className="row text-center mt-5 mb-2">
+        <h1>Technology</h1>
+
+        <h5 className="text-muted mt-2">
+          Sleek, modern and intuitive platform
+        </h5>
+
+        <br />
+        <br />
+
+        <p>
+          Check out our{" "}
+          <Link to="/signup" style={{ textDecoration: "none" }}>
+            Investment Offerings{" "}
+            <i
+              className="fa fa-long-arrow-right"
+              aria-hidden="true"
+            ></i>
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
