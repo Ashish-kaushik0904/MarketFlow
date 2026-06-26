a0f95d179d9fe685126636678f39847d15b7cc8d
Ashish <ashishkaushik0904@gmail.com>
2026-06-26 22:39:41 +0530

    Fix accessibility warnings

--- frontend/src/landing_page/home/Awards.js
+++ frontend/src/landing_page/home/Awards.js
@@ -1,48 +1,51 @@
-import React from 'react'
+import React from "react";
 
 function Awards() {
-    return (
-        <div className='container mt-5'>
-            <div className='row'>
-                <div className='col-6 p-5'>
-                    <img src='media/images/largestbroker.svg' />
-                </div>
-                <div className='col-6 p-5 mt-3'>
-                    <h1>Largest stock broker in India</h1>
-                    <p className='mb-5'>2+ million MarketFlow clients contribute to over 15% of all the volumes in India daily by trading and aianvestin in:</p>
-                    <div className='row'>
-                        <div className='col-6'>
-                            <ul>
-                                <li>
-                                    <p>Further and Options</p>
-                                </li>
-                                <li>
-                                    <p>Commodity Derivatves</p>
-                                </li>
-                                <li>
-                                    <p>Currency Derivative</p>
-                                </li>
-                            </ul>
-                        </div>
-                        <div className='col-6'>
-                             <ul>
-                                <li>
-                                    <p>Stocks & IPOs</p>
-                                </li>
-                                <li>
-                                    <p>Direct mutual funds</p>
-                                </li>
-                                <li>
-                                    <p>Bonds and Govt. Secutities</p>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <img src='media/images/pressLogos.png' style={{width:"90%"}} />
-                </div>
-            </div>
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-6 p-5">
+          <img
+            src="media/images/largestbroker.svg"
+            alt="Largest broker illustration"
+          />
         </div>
-    );
+
+        <div className="col-6 p-5 mt-3">
+          <h1>Largest stock broker in India</h1>
+
+          <p className="mb-5">
+            2+ million MarketFlow clients contribute to over 15% of all the
+            volumes in India daily by trading and investing in:
+          </p>
+
+          <div className="row">
+            <div className="col-6">
+              <ul>
+                <li><p>Futures and Options</p></li>
+                <li><p>Commodity Derivatives</p></li>
+                <li><p>Currency Derivatives</p></li>
+              </ul>
+            </div>
+
+            <div className="col-6">
+              <ul>
+                <li><p>Stocks & IPOs</p></li>
+                <li><p>Direct Mutual Funds</p></li>
+                <li><p>Bonds and Govt. Securities</p></li>
+              </ul>
+            </div>
+          </div>
+
+          <img
+            src="media/images/pressLogos.png"
+            alt="Media partners"
+            style={{ width: "90%" }}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Awards;
--- frontend/src/landing_page/home/Education.js
+++ frontend/src/landing_page/home/Education.js
@@ -1,24 +1,52 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Education() {
-    return ( 
-        <div className='container mb-5'>
-            <div className='row'>
-                <div className='col-6'>
-                   <img src='media/images/education.svg' style={{width: "70%"}} />
-                </div>
-                <div className='col-6 mb-5'>
-                     <h1 className='mb-3 fs-2'>Free and open market education</h1>
-                    <p>Varsity, the largest online stock market education book in the world covering everythings from the basics to advance trading.</p>
-                    <a href='' className='mx-4' style={{textDecoration:"none"}}>Versity <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+  return (
+    <div className="container mb-5">
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="media/images/education.svg"
+            alt="Education"
+            style={{ width: "70%" }}
+          />
+        </div>
 
-                    <p className='mt-5'>Trading Q&A, the most active trading and investment community in India for all your market related queries.</p>
-                    <a href='' className='mx-4' style={{textDecoration:"none"}}>Trading Q&A <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
-                </div>
-                
-             </div>
-         </div>
-     );
+        <div className="col-6 mb-5">
+          <h1 className="mb-3 fs-2">Free and open market education</h1>
+
+          <p>
+            Varsity, the largest online stock market education book in the
+            world covering everything from the basics to advanced trading.
+          </p>
+
+          <Link
+            to="/products"
+            className="mx-4"
+            style={{ textDecoration: "none" }}
+          >
+            Varsity{" "}
+            <i className="fa fa-long-arrow-right" aria-hidden="true"></i>
+          </Link>
+
+          <p className="mt-5">
+            Trading Q&A, the most active trading and investment community in
+            India for all your market related queries.
+          </p>
+
+          <Link
+            to="/support"
+            className="mx-4"
+            style={{ textDecoration: "none" }}
+          >
+            Trading Q&amp;A{" "}
+            <i className="fa fa-long-arrow-right" aria-hidden="true"></i>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Education;
