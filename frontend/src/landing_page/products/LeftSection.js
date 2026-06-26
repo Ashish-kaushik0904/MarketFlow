d94119422b09f25803be542ee3f5f3a5d379b6f5
Ashish <ashishkaushik0904@gmail.com>
2026-06-26 22:23:45 +0530

    Fix image alt attributes

--- frontend/src/landing_page/products/LeftSection.js
+++ frontend/src/landing_page/products/LeftSection.js
@@ -1,30 +1,66 @@
-import React from 'react'
+import React from "react";
 
-function LeftSection({imageURL, productName, productDesription, tryDemo, learnMore, googlePlay, appStore}) {
-    return ( 
-        <div className='container warp'>
-            <div className='row border-top'>
-                <div className='col-6' style={{paddingRight:"20px"}}>
-                    <img src={imageURL} className="img-fluid"/>
-                </div>
-                {/* <div className='col-2 mr-5'></div> */}
-                <div className='col-6 mt-5' style={{paddingLeft:"70px"}}>
-                    <h1>{productName}</h1>
-                    <p>{productDesription}</p>
-
-                    <div>
-                    <a href={tryDemo} style={{textDecoration:"none"}}>Try Demo <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a> 
-                    <a href={learnMore} style={{textDecoration:"none", marginLeft:"50px"}}>Learn more <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a> 
-                    </div>
-
-                    <div className='mt-3'>
-                    <a href={googlePlay}><img src="media/images/googlePlayBadge.svg" /></a> 
-                    <a href={appStore} style={{textDecoration:"none", marginLeft:"50px"}}><img src="media/images/appstoreBadge.svg" /> </a>
-                    </div>
-                </div>
-            </div>
+function LeftSection({
+  imageURL,
+  productName,
+  productDesription,
+  tryDemo,
+  learnMore,
+  googlePlay,
+  appStore,
+}) {
+  return (
+    <div className="container warp">
+      <div className="row border-top">
+        <div className="col-6" style={{ paddingRight: "20px" }}>
+          <img
+            src={imageURL}
+            alt={productName}
+            className="img-fluid"
+          />
         </div>
-     );
+
+        <div className="col-6 mt-5" style={{ paddingLeft: "70px" }}>
+          <h1>{productName}</h1>
+          <p>{productDesription}</p>
+
+          <div>
+            <a href={tryDemo} style={{ textDecoration: "none" }}>
+              Try Demo{" "}
+              <i className="fa fa-long-arrow-right" aria-hidden="true"></i>
+            </a>
+
+            <a
+              href={learnMore}
+              style={{ textDecoration: "none", marginLeft: "50px" }}
+            >
+              Learn more{" "}
+              <i className="fa fa-long-arrow-right" aria-hidden="true"></i>
+            </a>
+          </div>
+
+          <div className="mt-3">
+            <a href={googlePlay}>
+              <img
+                src="media/images/googlePlayBadge.svg"
+                alt="Get it on Google Play"
+              />
+            </a>
+
+            <a
+              href={appStore}
+              style={{ textDecoration: "none", marginLeft: "50px" }}
+            >
+              <img
+                src="media/images/appstoreBadge.svg"
+                alt="Download on the App Store"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default LeftSection;
--- frontend/src/landing_page/products/RightSection.js
+++ frontend/src/landing_page/products/RightSection.js
@@ -1,22 +1,39 @@
-import React from 'react'
+import React from "react";
 
-function RightSection({imageURL, productName, productDesription, tryDemo, learnMore, googlePlay, appStore}) {
-    return ( 
-        <div className='container border-top mt-5'>
-            <div className='row'>
-                <div className='col-6 mt-5'>
-                    <h1>{productName}</h1>
-                    <p>{productDesription}</p>
-                    <div>
-                        <a href={learnMore} style={{textDecoration:"none"}}>Learn more <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
-                    </div>
-                </div>
-                <div className='col-6'>
-                    <img src={imageURL} className="img-fluid"></img>
-                </div>
-            </div>
+function RightSection({
+  imageURL,
+  productName,
+  productDesription,
+  tryDemo,
+  learnMore,
+  googlePlay,
+  appStore,
+}) {
+  return (
+    <div className="container border-top mt-5">
+      <div className="row">
+        <div className="col-6 mt-5">
+          <h1>{productName}</h1>
+          <p>{productDesription}</p>
+
+          <div>
+            <a href={learnMore} style={{ textDecoration: "none" }}>
+              Learn more{" "}
+              <i className="fa fa-long-arrow-right" aria-hidden="true"></i>
+            </a>
+          </div>
         </div>
-     );
+
+        <div className="col-6">
+          <img
+            src={imageURL}
+            alt={productName}
+            className="img-fluid"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default RightSection;
