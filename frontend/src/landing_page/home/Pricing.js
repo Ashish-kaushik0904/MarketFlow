b5895dcbf0dee9260db779e308668283948e8e94
Ashish <ashishkaushik0904@gmail.com>
2026-06-26 22:35:01 +0530

    Fix HomePage and Pricing

--- frontend/src/landing_page/home/Pricing.js
+++ frontend/src/landing_page/home/Pricing.js
@@ -1,30 +1,53 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Pricing() {
-    return ( 
-        <div className='container'>
-            <div className='row'>
-                <div className='col-4'>
-                    <h1 className='mb-3 fs-2'>Unbeatable Pricing</h1>
-                    <p>We pioneered the concept of discount broking price transparency in India. Flat fees and no hidden charges.</p>
-                    <a href='' className='mx-4' style={{textDecoration:"none"}}>See Pricing <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
-                </div>
-                <div className='col-2'></div>
-                <div className='col-6'>
-                    <div className='row text-center'>
-                        <div className='col border p-2'>
-                            <h1 className='mb-3'>₹0</h1>
-                            <p>Free equity delivery and <br/>direct mutual funds</p>
-                        </div>
-                        <div className='col border p-2'>
-                            <h1>₹20</h1>
-                            <p>Intreaday and F&0</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-4">
+          <h1 className="mb-3 fs-2">Unbeatable Pricing</h1>
+
+          <p>
+            We pioneered the concept of discount broking with transparent
+            pricing. Flat fees and no hidden charges.
+          </p>
+
+          <Link
+            to="/pricing"
+            className="mx-4"
+            style={{ textDecoration: "none" }}
+          >
+            See Pricing{" "}
+            <i
+              className="fa fa-long-arrow-right"
+              aria-hidden="true"
+            ></i>
+          </Link>
         </div>
-     );
+
+        <div className="col-2"></div>
+
+        <div className="col-6">
+          <div className="row text-center">
+            <div className="col border p-2">
+              <h1 className="mb-3">₹0</h1>
+              <p>
+                Free equity delivery and
+                <br />
+                direct mutual funds
+              </p>
+            </div>
+
+            <div className="col border p-2">
+              <h1>₹20</h1>
+              <p>Intraday and F&O</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Pricing;
